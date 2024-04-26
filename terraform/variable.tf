17439ef8ebd3a1d0f503fb57b79a032b0fcfadd0
--unset <jyotibind09@gmail.com>
2024-04-26 19:41:14 +0530

    test added role removed deny

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,5 +1,5 @@
-variable "ecs_task_execution_role_name" {
-  description = "ECS task execution role name"
-  default     = "node_app_Role"
-  type        = string
-}
+# variable "ecs_task_execution_role_name" {
+#   description = "ECS task execution role name"
+#   default     = "node_app_Role"
+#   type        = string
+# }
