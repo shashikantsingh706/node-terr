105af95a686a929ab490ba55c8cb0a72397deec0
shashikantsingh706 <66770585+shashikantsingh706@users.noreply.github.com>
2024-04-26 19:23:45 +0530

    Test (#28)
    * mm
    
    * test final
    
    * test added role
    
    ---------
    
    Co-authored-by: --unset <jyotibind09@gmail.com>

---
+++ terraform/iam.tf
@@ -0,0 +1,59 @@
+data "aws_iam_policy_document" "node_app_task_execution_role" {
+  version = "2012-10-17"
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+# resource "aws_iam_policy" "cloud_watch" {
+#   name = "axle-form-cloudwatch-policy"
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Action" : [
+#           "logs:DescribeLogGroups",
+#           "logs:DescribeLogStreams",
+#           "logs:CreateLogGroup",
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents",
+#           "logs:PutRetentionPolicy"
+#         ],
+#         "Resource" : "*",
+#         "Effect" : "Allow"
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_policy" "secretsmanager" {
+#   name = "axle-form-secretmanager-policy"
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Action" : [
+#           "secretsmanager:*"
+#         ],
+#         "Resource" : "*",
+#         "Effect" : "Allow"
+#       }
+#     ]
+#   })
+# }
+
+resource "aws_iam_role" "node_app_task_execution_role" {
+  name               = var.ecs_task_execution_role_name
+  assume_role_policy = data.aws_iam_policy_document.node_app_task_execution_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_access_policy" {
+  role       = aws_iam_role.node_app_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
