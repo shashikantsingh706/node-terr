1cc0cb4647bedc124dec26b696679583c19181cf
--unset <jyotibind09@gmail.com>
2024-05-06 16:14:12 +0530

    added permission

--- terraform/repository.tf
+++ terraform/repository.tf
@@ -7,20 +7,36 @@ resource "aws_ecr_repository" "repo" {
   }
 }
 
-resource "aws_ecr_repository_policy" "repo_policy" {
-  repository = aws_ecr_repository.repo.name
+data "aws_iam_policy_document" "repo" {
+  statement {
+    sid    = "new policy"
+    effect = "Allow"
 
-  policy = jsonencode({
-    Version = "2008-10-17"
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = {
-          AWS = "arn:aws:iam::891377310564:user/shashikant"
-        },
-        Action    = "ecr:InitiateLayerUpload",
-        Resource  = aws_ecr_repository.repo.arn
-      }
+    principals {
+      type        = "AWS"
+      identifiers = ["891377310564"]
+    }
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
     ]
-  })
+  }
+}
+
+resource "aws_ecr_repository_policy" "repo" {
+  repository = aws_ecr_repository.repo.name
+  policy     = data.aws_iam_policy_document.repo.json
 }
