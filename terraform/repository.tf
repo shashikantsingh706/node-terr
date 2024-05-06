936d86795801de6e7ab50411a10c06e6462f69da
shashikantsingh706 <66770585+shashikantsingh706@users.noreply.github.com>
2024-05-06 16:03:48 +0530

    Test (#39)
    * test for
    
    * test for
    
    * test for
    
    * login added
    
    * login added
    
    * login added
    
    * login added
    
    * login added
    
    * added permission
    
    * added permission
    
    ---------
    
    Co-authored-by: --unset <jyotibind09@gmail.com>

--- terraform/repository.tf
+++ terraform/repository.tf
@@ -6,3 +6,21 @@ resource "aws_ecr_repository" "repo" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository_policy" "repo_policy" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::891377310564:user/shashikant"
+        },
+        Action    = "ecr:InitiateLayerUpload",
+        Resource  = aws_ecr_repository.repo.arn
+      }
+    ]
+  })
+}
