f541db86249a50824f8b6f543092de2a955b61be
shashikantsingh706 <66770585+shashikantsingh706@users.noreply.github.com>
2024-05-06 16:44:49 +0530

    Test (#42)
    * test for
    
    * test for
    
    * test for
    
    * login added
    
    * login added
    
    * login added
    
    * login added
    
    * login added
    
    * added permission
    
    * added permission
    
    * added permission
    
    * added permission
    
    * added
    
    * added
    
    ---------
    
    Co-authored-by: --unset <jyotibind09@gmail.com>

--- terraform/repository.tf
+++ terraform/repository.tf
@@ -7,36 +7,4 @@ resource "aws_ecr_repository" "repo" {
   }
 }
 
-data "aws_iam_policy_document" "repo" {
-  statement {
-    sid    = "new policy"
-    effect = "Allow"
 
-    principals {
-      type        = "AWS"
-      identifiers = ["891377310564"]
-    }
-
-    actions = [
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:PutImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:CompleteLayerUpload",
-      "ecr:DescribeRepositories",
-      "ecr:GetRepositoryPolicy",
-      "ecr:ListImages",
-      "ecr:DeleteRepository",
-      "ecr:BatchDeleteImage",
-      "ecr:SetRepositoryPolicy",
-      "ecr:DeleteRepositoryPolicy",
-    ]
-  }
-}
-
-resource "aws_ecr_repository_policy" "repo" {
-  repository = aws_ecr_repository.repo.name
-  policy     = data.aws_iam_policy_document.repo.json
-}
