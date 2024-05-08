07da4ff7608624699adf99a55d8c5992745281bb
--unset <shashikantsingh706@outlook.com>
2024-05-08 11:16:38 +0530

    added alert rule

---
+++ terraform/alert.tf
@@ -0,0 +1,22 @@
+# Create CloudWatch Events Rule for ECS task failure or stop
+resource "aws_cloudwatch_event_rule" "ecs_task_failure_rule" {
+  name                = "ecs_task_failure_rule"
+  description         = "Trigger on ECS task failures or stops"
+  event_pattern       = <<PATTERN
+{
+  "source": ["aws.ecs"],
+  "detail-type": ["ECS Task State Change"],
+  "detail": {
+    "lastStatus": ["STOPPED", "FAILED", "DELETED"]
+  }
+}
+PATTERN
+}
+
+# Associate CloudWatch Events Rule with existing SNS Topic
+resource "aws_cloudwatch_event_target" "ecs_task_failure_target" {
+  rule      = aws_cloudwatch_event_rule.ecs_task_failure_rule.name
+  target_id = "send_notification"
+
+  arn = "arn:aws:sns:us-east-1:891377310654:ecs_failed_task_topic"
+}
