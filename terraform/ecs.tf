77f39f3d03fbd420df933c9fd1af5cd8e05207d4
--unset <shashikantsingh706@outlook.com>
2024-05-08 13:56:20 +0530

    mofied IAM for cloudwatvch syntax

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -42,6 +42,6 @@ resource "aws_ecs_task_definition" "td" {
   cpu                = "256"
   memory             = "512"
   network_mode       = "awsvpc"
-  task_role_arn      = "arn:aws:iam::891377310654:role/ecsTaskExecutionRole"
-  execution_role_arn = "arn:aws:iam::891377310654:role/ecsTaskExecutionRole"
+  task_role_arn      = aws_iam_role.node_app_task_execution_role
+  execution_role_arn = aws_iam_role.node_app_task_execution_role
 }
